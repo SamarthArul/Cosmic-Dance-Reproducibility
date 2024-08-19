70380d702761f053df3d9419e86ec6024413dcdf
Suvam Basak <suvambasak2008@gmail.com>
2024-08-19 20:58:35 +0530

    Dataset pre processing

--- pre_process/cleanup.py
+++ pre_process/cleanup.py
@@ -1,16 +1,44 @@
+'''Remove satellite records based on criteria'''
+
+
 from cosmic_dance.data_processor import *
+import concurrent.futures
 
-TLE_DIR_CSV = '/mnt/Storage/OUTPUTs/TLEs'
 
-for file in get_file_names(TLE_DIR_CSV):
-    file_path = f"{TLE_DIR_CSV}/{file}"
+def clean_up(file_path: str) -> None:
+    '''Remove the file if not match criteria
+
+    Params
+    ------
+    file_path: str
+        Path to the CSV file
+    '''
+
     df = read_CSV(file_path)
 
     # Removing file with less than 10 TLEs
     if len(df) < 10:
-        print(f" > Remove: {file_path} < 10 TLEs")
+        print(f"|- Remove: {file_path} < 10 TLEs")
         remove_file(file_path)
 
+    # Removing file with satellite age below 30 days
     elif satellite_age_in_days(df) < 30:
-        print(f" > Remove: {file_path} < 30 days")
+        print(f"|- Remove: {file_path} < 30 days")
         remove_file(file_path)
+
+
+if __name__ == '__main__':
+    TLE_DIR_CSV = '/mnt/Storage/OUTPUTs/Starlink/TLEs'
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+
+        # For each file in that dir
+        for file in get_file_names(TLE_DIR_CSV):
+            file_path = f"{TLE_DIR_CSV}/{file}"
+
+            executor.submit(
+                clean_up, file_path
+            )
+
+        executor.shutdown()
+        print(f'|\n|- Complete.')
