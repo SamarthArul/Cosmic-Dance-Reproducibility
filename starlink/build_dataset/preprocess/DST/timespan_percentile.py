caf111fe7bbd9a568bc0ea4fd9b659f64f4b88cb
Suvam Basak <suvambasak2008@gmail.com>
2024-09-04 16:33:14 +0530

    Preprocess solar events

---
+++ starlink/build_dataset/preprocess/DST/timespan_percentile.py
@@ -0,0 +1,53 @@
+'''Extracting the duration of solar activity
+
+- Above X %tile
+- Merging two duration closer then Y days
+
+'''
+
+
+from cosmic_dance.dst_index import *
+from cosmic_dance.io import *
+from cosmic_dance.stats import *
+
+OUTPUT_DIR = f"{OUTPUT_DIR}/Starlink/timespans/percentile"
+create_directories(OUTPUT_DIR)
+
+# INPUT FILE(s)
+DST_CSV = "artifacts/DST/Dst_index.csv"
+
+
+# OUTPUT FILE(s)
+
+# Solar activity window above the 80, 95, and 99 %tile
+DST_TIMESPAN_PTILE_99 = f"{OUTPUT_DIR}/above_ptile_99.csv"
+DST_TIMESPAN_PTILE_95 = f"{OUTPUT_DIR}/above_ptile_95.csv"
+DST_TIMESPAN_PTILE_80 = f"{OUTPUT_DIR}/above_ptile_80.csv"
+
+# Merging the solar activity window closer than 10 days
+MERGED_DST_TIMESPAN_PTILE_80 = f"{OUTPUT_DIR}/merged_above_ptile_80.csv"
+MERGED_DST_TIMESPAN_PTILE_95 = f"{OUTPUT_DIR}/merged_above_ptile_95.csv"
+MERGED_DST_TIMESPAN_PTILE_99 = f"{OUTPUT_DIR}/merged_above_ptile_99.csv"
+
+# Above 80, 95, 99 %tile
+df_dst = read_dst_index_CSV(DST_CSV)
+for ptile, filename in [
+    (99, DST_TIMESPAN_PTILE_99),
+    (95, DST_TIMESPAN_PTILE_95),
+    (80, DST_TIMESPAN_PTILE_80)
+]:
+    df = extract_timespan_above_nT_intensity(
+        df_dst, percentile(df_dst["nT"], ptile)
+    )
+    df = add_window_duration(df)
+    export_as_csv(df, filename)
+
+# Merge windows closer than 10 days
+for unmerged_fname, merged_fname in [
+    (DST_TIMESPAN_PTILE_80, MERGED_DST_TIMESPAN_PTILE_80),
+    (DST_TIMESPAN_PTILE_95, MERGED_DST_TIMESPAN_PTILE_95),
+    (DST_TIMESPAN_PTILE_99, MERGED_DST_TIMESPAN_PTILE_99)
+]:
+    df = read_timespan_CSV(unmerged_fname)
+    df = merge_window(df, 10)
+    export_as_csv(df, merged_fname)
---
+++ starlink/build_dataset/preprocess/DST/timespan_quiet_day.py
@@ -0,0 +1,45 @@
+'''Extracting the duration of low solar activity
+
+- Below X %tile
+- Merging two duration closer then Y days
+
+'''
+
+
+from cosmic_dance.dst_index import *
+from cosmic_dance.io import *
+from cosmic_dance.stats import *
+
+OUTPUT_DIR = f"{OUTPUT_DIR}/Starlink/timespans/quiet_day"
+create_directories(OUTPUT_DIR)
+
+# INPUT FILE(s)
+DST_CSV = "artifacts/DST/Dst_index.csv"
+
+
+# OUTPUT FILE(s)
+
+# Intensity below 80 %tile for atleast 13 days
+DST_TIMESPAN_PTILE_80 = f"{OUTPUT_DIR}/below_ptile_80.csv"
+
+# Merging the solar activity window closer than 10 days
+MERGED_DST_TIMESPAN_PTILE_80 = f"{OUTPUT_DIR}/merged_below_ptile_80.csv"
+
+
+df_dst = read_dst_index_CSV(DST_CSV)
+
+# Below 80 %tile
+df = extract_timespan_below_nT_intensity(
+    df_dst,
+    percentile(df_dst[DST.NANOTESLA], 80)
+)
+
+# Filter duration above 13 days
+df = add_window_duration(df)
+df = df[df[DST.DURATION_HOURS] > 13*24]
+export_as_csv(df, DST_TIMESPAN_PTILE_80)
+
+# Merge windows closer than 10 days
+df = read_timespan_CSV(DST_TIMESPAN_PTILE_80)
+df = merge_window(df, 10)
+export_as_csv(df, MERGED_DST_TIMESPAN_PTILE_80)
