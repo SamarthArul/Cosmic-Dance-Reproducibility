4c3cf8cdd9af63940bdf2925f488d45b1d0c0b18
Suvam Basak <suvambasak2008@gmail.com>
2024-09-04 17:23:11 +0530

    Preprocess Starlink TLEs

---
+++ starlink/build_dataset/preprocess/TLEs/JSON_to_CSV.py
@@ -0,0 +1,60 @@
+'''
+Convert TLEs from JSON to CSV format
+while removing some attributes if not required for the analysis
+'''
+
+
+import concurrent.futures
+
+from cosmic_dance.io import *
+from cosmic_dance.TLEs import *
+
+OUTPUT_DIR = f"{OUTPUT_DIR}/Starlink"
+
+PARALLEL_MODE = True
+
+
+# INPUT FILE(s)
+
+# JSON file Directory
+JSON_DIR = f"{OUTPUT_DIR}/RAW_TLEs"
+
+
+# OUTPUT FILE(s)
+
+# OUTPUT - CSV file Directory
+CSV_DIR = f"{OUTPUT_DIR}/TLEs"
+
+
+recreate_directories(CSV_DIR)
+
+_count = 0
+if PARALLEL_MODE:
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+
+        # for each JSON file in input dir
+        for file in get_file_names(JSON_DIR):
+            input_file_path = f"{JSON_DIR}/{file}"
+            output_file_path = f"{CSV_DIR}/{file.split('.')[0]}.csv"
+            _count += 1
+            print(f"|- Processing {file}")
+
+            executor.submit(
+                convert_from_JSON_to_CSV, input_file_path, output_file_path
+            )
+
+        executor.shutdown()
+
+# Serial mode
+else:
+
+    # for each JSON file in input dir
+    for file in get_file_names(JSON_DIR):
+        input_file_path = f"{JSON_DIR}/{file}"
+        output_file_path = f"{CSV_DIR}/{file.split('.')[0]}.csv"
+        _count += 1
+        print(f"|- Processing {file}")
+
+        convert_from_JSON_to_CSV(input_file_path, output_file_path)
+
+print(f"|\n|- Complete processing {_count} JSON file(s).")
---
+++ starlink/build_dataset/preprocess/TLEs/remove_orbit_raise_maneuver.py
@@ -0,0 +1,89 @@
+'''Removing the TLEs before satellites deployed into designated operational altitude'''
+
+
+import concurrent.futures
+
+from cosmic_dance.io import *
+from cosmic_dance.TLEs import *
+
+
+def strip_orbit_raise_maneuver(filename: str, orbit_raise_df: pd.DataFrame):
+    '''Remove all the TLEs before ORBIT_RAISE_COMEPLETE
+
+    Params
+    ------
+    filename: str
+        CSV file (NORAD_CAT_ID.csv)
+    orbit_raise_df: pd.DataFrame
+        DataFrame of orbit raise
+    '''
+
+    df_TLE = read_TLEs_in_CSV(filename)
+
+    # Get the launch date
+    launch_date = df_TLE.iloc[-1][TLE.LAUNCH_DATE]
+
+    # Query the row
+    row = orbit_raise_df[orbit_raise_df[TLE.LAUNCH_DATE] == launch_date]
+
+    # Found a record, remove TLEs upto orbit raise complete
+    if len(row) == 1:
+
+        maneuver_complete_date = row.iloc[-1]["ORBIT_RAISE_COMEPLETE"]
+
+        df_TLE = df_TLE[df_TLE[TLE.EPOCH] > maneuver_complete_date]
+        export_as_csv(df_TLE, filename)
+
+        print(f'|- UPDATED:{launch_date} > {filename}')
+
+    # Record not found, new launch remove the file
+    else:
+
+        remove_file(filename)
+        print(f'|- REMOVED:{launch_date} > {filename}')
+
+
+if __name__ == "__main__":
+
+    OUTPUT_DIR = f"{OUTPUT_DIR}/Starlink"
+
+    PARALLEL_MODE = True
+
+    # INPUT FILE(s)
+    TLE_CSV_DIR = f"{OUTPUT_DIR}/TLEs"
+    ORBIT_RAISE_CSV = "artifacts/starlink_orbit_raise.csv"
+
+    # OUTPUT FILE(s)
+    TLE_CSV_DIR = f"{OUTPUT_DIR}/TLEs"
+
+    # Confirm
+    input(f" File(s) in ({TLE_CSV_DIR}) might get altered? ")
+
+    orbit_raise_df = read_orbit_raise_CSV(ORBIT_RAISE_CSV)
+
+    # Complete path to all CSV files
+    list_of_sat_TLEs_in_CSV = [
+        f'{TLE_CSV_DIR}/{csv_filename}' for csv_filename in get_file_names(TLE_CSV_DIR)
+    ]
+
+    if PARALLEL_MODE:
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+
+            # For all satellites
+            for sat_TLEs_in_CSV in list_of_sat_TLEs_in_CSV:
+
+                executor.submit(
+                    strip_orbit_raise_maneuver,
+                    sat_TLEs_in_CSV, orbit_raise_df
+                )
+
+            executor.shutdown()
+
+    # Serial mode
+    else:
+
+        # For all satellites
+        for sat_TLEs_in_CSV in list_of_sat_TLEs_in_CSV:
+            strip_orbit_raise_maneuver(sat_TLEs_in_CSV, orbit_raise_df)
+
+    print('|\n|- Comeplete.')
