34629fcf38169474b46d2c14cf72a6c010055691
Suvam Basak <suvambasak2008@gmail.com>
2024-09-05 11:05:40 +0530

    Measure altitude change due to solar activity intensity and event duration

--- cosmic_dance/measurement.py
+++ cosmic_dance/measurement.py
@@ -97,3 +97,84 @@ def track_satellite_altitude_change(
                 },
                 out_filename
             )
+
+
+def maximum_altitude_difference(
+        event_date: pd.Timestamp,
+        observation_days: list[int],
+
+        tle_filename: str,
+        out_filename: str,
+
+        change_thershold: float = 5.0
+):
+    '''Measure maximum altitude difference observed of a given satellite by next a few observation days
+
+    Params
+    ------
+    event_date: pd.Timestamp
+        Timestamp of start date
+    observation_days: list[int]
+        Observation window length in days from start date
+    tle_filename: str
+        TLE CSV file name
+    out_filename: str
+        Output CSV file name
+    change_thershold: float, optional
+        Altitude change thershold (Default 0.5 KM)
+
+    '''
+
+    df_tle = read_TLEs_in_CSV(tle_filename)
+    tle_before_dict = get_last_TLE_before_the_date(df_tle, event_date)
+
+    # If not last TLE not found skip this satellite
+    # If already altitude changed (way below median) a lot then skip this satellite
+    if tle_before_dict is None:
+        return
+    median_altitude_before_event = get_median_altitude(df_tle, event_date)
+    if math.isnan(median_altitude_before_event):
+        return
+    if abs(median_altitude_before_event-tle_before_dict.get(TLE.ALTITUDE_KM)) >= change_thershold:
+        return
+
+    # Build a record
+    record = {
+        "EVENT_DATE": event_date,
+        "NORAD_CAT_ID": tle_before_dict.get(TLE.NORAD_CAT_ID),
+        "LAUNCH_DATE": tle_before_dict.get(TLE.LAUNCH_DATE),
+    }
+
+    # Measure the altitude change over next few days
+    for day_count in observation_days:
+        end_date = event_date+pd.Timedelta(days=day_count)
+
+        # Get the first TLE after the edate
+        tle_after_dict = get_first_TLE_after_the_date(df_tle, end_date)
+
+        # The satellite disappeared i.e, no TLEs after end_date
+        if tle_after_dict is None:
+            record[f"KM_after_DAY_{day_count}"] = 0
+            print(
+                f"""|- {tle_before_dict.get(TLE.NORAD_CAT_ID)} Gone after {event_date}"""
+            )
+            continue
+
+        # If not disappeared then get all the TLEs within that window (event_date to end_date)
+        df_tle_after_effect = get_all_TLE_between_two_date(
+            df_tle,
+            tle_before_dict.get(TLE.EPOCH),
+            tle_after_dict.get(TLE.EPOCH)
+        )
+
+        # Take the absolute drag and altitude difference
+        absolute_drag = df_tle_after_effect[TLE.DRAG].abs()
+        absolute_altitude_change = (
+            median_altitude_before_event - df_tle_after_effect[TLE.ALTITUDE_KM]
+        ).abs()
+
+        # Add the measurement to the record
+        record[f"KM_after_DAY_{day_count}"] = absolute_altitude_change.max()
+        record[f"DRAG_after_DAY_{day_count}"] = absolute_drag.max()
+
+    CSV_logger(record, out_filename)
